f8dd624e2b87028ed2ca16bd0579500afdbc892d
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2022-05-25 18:56:36 +0300

    fetching single quote

---
+++ pages/quotes/[id].js
@@ -0,0 +1,39 @@
+export const getStaticPaths = async () => {
+    const res = await fetch('https://quotable.io/quotes')
+   const data = await res.json()
+
+   const paths = data.results.map(quote => {
+       return {
+           params: { id: quote._id.toString() }
+       }
+   })
+
+   return {
+       paths,
+       fallback: false
+   } 
+}
+
+export const getStaticProps = async (context) => {
+    const id = context.params.id
+    const res = await fetch(`https://quotable.io/quotes/${id}`)
+    const data = await res.json()
+
+    return {
+        props: {quote: data}
+    }
+    
+ }
+
+const Details = ({quote}) => {
+
+    return ( 
+        <div>
+            <h1>{quote.content}</h1>
+            <h3>{quote.author}</h3>
+            <p>tags: {quote.tags.join(", ")}</p>
+        </div>
+     );
+}
+ 
+export default Details;
--- pages/quotes/index.js
+++ pages/quotes/index.js
@@ -1,9 +1,28 @@
-const Ninjas = () => {
+import styles from '../../styles/Quotes.module.css'
+import Link from 'next/link'
+
+export const getStaticProps = async () => {
+   const res = await fetch('https://quotable.io/quotes?page=1')
+   const data = await res.json()
+
+   return {
+       props: {quotes: data.results}
+   }
+   
+}
+
+const Quotes = ({quotes}) => {
+    
     return ( 
         <div>
             <h1>All Quotes</h1>
+            {quotes.map(quote => (
+                <Link href={`/quotes/${quote._id}`} key={quote._id}>
+                    <a className={styles.single}><h3>{quote.content}</h3></a>
+                </Link>
+            ))}
         </div>
     );
 }
  
-export default Ninjas;
+export default Quotes;
